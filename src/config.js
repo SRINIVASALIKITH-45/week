1d1d8d9aa14fda95067a07d699071d04cdc90ef2
SRINIVASALIKITH <likithmangapuram@gmail.com>
2026-04-13 12:52:43 +0530

    add tailwind dependencies

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = 'https://food-backend-production-958c.up.railway.app';
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://food-backend-production-a5b7.up.railway.app';
 export const API_URL = `${API_BASE_URL}/api`;
 export const SOCKET_URL = API_BASE_URL;
 export const IMAGE_BASE_URL = API_BASE_URL;
